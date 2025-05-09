41e459c2bbc882c9c859baad4a665ea3df9deb9a
Sushib08 <maevalagrost@gmail.com>
2025-05-09 15:45:32 +0200

    feat: dynamically fetch all products from MongoDB on home page

--- server/src/routes/productRoutes.ts
+++ server/src/routes/productRoutes.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import { getProductsByCategory } from "../controllers/productController";
+import {
+  getAllProducts,
+  getProductsByCategory,
+} from "../controllers/productController";
 
 const router = express.Router();
 
 router.get("/category/:categorySlug", getProductsByCategory);
+router.get("/", getAllProducts);
 
 export default router;
